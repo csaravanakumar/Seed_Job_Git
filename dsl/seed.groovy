ed10451c635c88416cdc84ce62a602fd6a4ee88a
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-03 19:25:55 -0700

    Update seed.groovy

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -1,5 +1,34 @@
-job('example') {
-  steps {
-    shell('echo Vrnaplex1')
-  }
+def createDeploymentJob(jobName, repoUrl) {
+    pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                        }
+                        branches('master')
+                        extensions {
+                            cleanBeforeCheckout()
+                        }
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
+}
+
+def createTestJob(jobName, repoUrl) {
+    multibranchPipelineJob(jobName) {
+        branchSources {
+            git {
+                remote(repoUrl)
+                includes('*')
+            }
+        }
+        triggers {
+            cron("H/5 * * * *")
+        }
+    }
 }
